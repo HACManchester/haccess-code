3ba082200ec6a1f2d5bc4663cdb3a376b37147cf
Ben Dooks <ben-linux@fluff.org>
2016-04-03 13:34:01 +0100

    haccess: url watch code

---
+++ urlwatch.cpp
@@ -0,0 +1,49 @@
+
+#include <ESP8266WiFi.h>
+#include "urlwatch.h"
+
+static const bool debug = false;
+
+enum url_result UrlWatch::upToDate(void)
+{
+  enum url_result ret = URL_SAME;
+  bool got_modified = false;
+  WiFiClient client;
+  String line;
+
+  if (!client.connect(this->host, this->port)) {
+    return URL_FETCH_ERR;
+  }
+
+  client.print("HEAD ");
+  client.print(this->url);
+  client.print(" HTTP/1.0\r\n");
+  client.print("Host: ");
+  client.printf(this->host);
+  client.print("\r\n\r\n");
+
+  while (client.connected()) {
+    if (!client.available())
+      continue;
+
+    line = client.readStringUntil('\n');
+    if (debug)
+      Serial.println("line: " + line);
+
+    if (line.startsWith("Last-Modified:")) {
+      String lmdate = line.substring(line.indexOf(" "));
+      got_modified = true;
+
+      if (!lmdate.equals(this->last)) {
+        this->last = lmdate;
+        ret = URL_CHANGED;
+      }
+    }
+  }
+
+  if (!got_modified)
+    ret = URL_FETCH_ERR;
+
+  return ret;
+}
+
---
+++ urlwatch.h
@@ -0,0 +1,31 @@
+// Haccess v1 Access Control System
+// Copyright 2015 Ben Dooks <ben@fluff.org>
+
+enum url_result {
+  URL_FETCH_ERR,
+  URL_CHANGED,
+  URL_SAME
+};
+
+class UrlWatch {
+ public:
+  UrlWatch(const char *host, unsigned int port, const char *url) {
+    this->host = host;
+    this->port = port;
+    this->url = url;
+  }
+
+  enum url_result upToDate(void);
+
+  const char *getHost() { return this->host; }
+  const char *getUrl() { return this->url; }
+  const unsigned int getPort() { return this->port; };
+  
+ private:
+  String last;			// last modification date from host
+  
+  const char *host;
+  const char *url;
+  unsigned int port;
+
+};
