c647fbdf978f050c2769685f717b8efb26af023b
Ben Dooks <ben-linux@fluff.org>
2016-09-27 23:37:01 +0100

    move config struct into config.cpp

--- config.cpp
+++ config.cpp
@@ -10,3 +10,18 @@ IniFile cfgfile;
 
 
 
+
+struct config cfg = {
+  .en_rfid = true,
+  .en_mqtt = false,
+  .en_cards = false,
+  .en_cards_fetch = false,
+  .en_cards_update = false,
+  .rfid_interval = 250,
+  .wifi_ssid = "Hackspace",
+  .wifi_pass = "T3h4x0rZ",
+  .card_host = NULL,
+  .card_url = NULL,
+  .mqtt_server = NULL,
+  .mqtt_port = 1883,
+};
